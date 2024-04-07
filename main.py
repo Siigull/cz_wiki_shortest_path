ae43cc40607c6d369eab3f4e78ccc77645c2ff8b
Siigull <72805357+Siigull@users.noreply.github.com>
2024-04-07 13:40:08 +0200

    refactoring

--- main.py
+++ main.py
@@ -8,12 +8,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 from collections import deque
 
+######## variable init #########
 start = "/wiki/Fakulta_informa%C4%8Dn%C3%ADch_technologi%C3%AD_Vysok%C3%A9ho_u%C4%8Den%C3%AD_technick%C3%A9ho_v_Brn%C4%9B"
 end = "/wiki/Klinefelter%C5%AFv_syndrom"
 map = {}
 map[start] = []
 q = deque()
 vis = set()
+################################
+
+def parse_html(link):
+    page = requests.get("https://cs.wikipedia.org/" + link)
+    data = page.text
+    soup = BeautifulSoup(data)
+
+    return soup
+
+def is_article(link):
+    if link.startswith("/wiki/Speci") or \
+       link.startswith("/wiki/Wikipedie") or \
+       link.startswith("/wiki/N%C3%A1pov%C4%9Bda") or \
+       link.startswith("/wiki/Hlavn%C3%AD_strana") or \
+       link.startswith("/wiki/Soubor") or \
+       link.startswith("/wiki/Kategorie"):
+        return False
+    else:
+        return True
 
 ######## one depth search from end ########
 # have to use selenium because the returned html from links from on wikipedia doesnt work
@@ -32,27 +52,17 @@ def get_backwards_reference(search_link):
     for link in driver.find_elements(By.TAG_NAME, "a"):
         cur_link = link.get_attribute('href')
 
-        if cur_link == None:
-            break
+        if cur_link != None:
+            cur_link = cur_link[24:] ## remove https://cs.wikipedia.org shown because of view-source
 
-        if cur_link in vis_end:
-            continue
+            if cur_link.startswith("/wiki/"):
+                if cur_link not in vis_end:
+                    if is_article(cur_link):
+                        vis_end.add(cur_link)
 
-        vis_end.add(cur_link)
+                        referencing_end.add(cur_link)
 
-        cur_link = cur_link[24:]
-        if cur_link.startswith("/wiki/Speci") or \
-        cur_link.startswith("/wiki/Wikipedie") or \
-        cur_link.startswith("/wiki/N%C3%A1pov%C4%9Bda") or \
-        cur_link.startswith("/wiki/Hlavn%C3%AD_strana") or \
-        cur_link.startswith("/wiki/Soubor") or \
-        cur_link.startswith("/wiki/Kategorie"):
-            continue
-
-        if cur_link.startswith("/wiki/"):
-            referencing_end.add(cur_link)
-
-            end_metadata[cur_link] = search_link
+                        end_metadata[cur_link] = search_link
 
 get_backwards_reference(end)
 
@@ -62,73 +72,73 @@ if len(referencing_end) < 50:
         get_backwards_reference(i)
 
 driver.quit()
+
+def print_end_reference(link):
+    print_arr = []
+    print_arr.append(link)
+
+    while end_metadata[link] != end:
+        print_arr.append(end_metadata[link])
+        link = end_metadata[link]
+    
+    print(end)
+    for i in reversed(print_arr):
+        print(i)
 #############################################
+
 
 def get_links(start_link):
     print("currently parsing:" + start_link)
 
-    page = requests.get("https://cs.wikipedia.org/" + start_link)
-    data = page.text
-    soup = BeautifulSoup(data)
+    soup = parse_html(start_link)
 
     for link in soup.find_all('a'):
         cur_link = link.get('href')
 
-        if cur_link == None:
-            break
+        if cur_link != None:
+            if cur_link.startswith("/wiki/"):
+                if cur_link not in vis:
+                    if is_article(cur_link):
+                        vis.add(cur_link)
+                        q.append(cur_link)
 
-        if cur_link.startswith("/wiki/"):
-            if cur_link in vis:
-                continue
+                        map[start_link].append(cur_link)
 
-            if cur_link.startswith("/wiki/Speci") or \
-                cur_link.startswith("/wiki/Wikipedie") or \
-                cur_link.startswith("/wiki/N%C3%A1pov%C4%9Bda") or \
-                cur_link.startswith("/wiki/Hlavn%C3%AD_strana") or \
-                cur_link.startswith("/wiki/Soubor") or \
-                cur_link.startswith("/wiki/Kategorie"):
-                    continue
-            
-            vis.add(cur_link)
-            q.append(cur_link)
+                        map[cur_link] = []
+                        map[cur_link].append("head" + start_link)
 
-            map[start_link].append(cur_link)
+                        if cur_link in referencing_end:
+                            print_end_reference(cur_link)
 
-            map[cur_link] = []
-            map[cur_link].append("head" + start_link)
-            if cur_link in referencing_end:
-                print_arr = []
-                print_arr.append(cur_link)
-                print(end)
-                while end_metadata[cur_link] != end:
-                    print_arr.append(end_metadata[cur_link])
-                    cur_link = end_metadata[cur_link]
-                
-                for i in reversed(print_arr):
-                    print(i)
-
-                return "found"
-        
+                            return "found"
     return ""
 
+
 ######## start of main ########
+cur_link = ""
 
 q.append(start)
-
-cur_link = ""
+vis.add(start)
 
 while True:
     cur_link = q.popleft()
 
+    if cur_link in referencing_end:
+        print_end_reference(cur_link)
+        cur_link = ""
+
+        break
+
     if get_links(cur_link) == "found":
         break
 
-while True:
-    print(cur_link)
-    
-    if(cur_link == start):
-        break
+if cur_link != "":
+    while True:
+        print(cur_link)
+        
+        if(cur_link == start):
+            break
 
-    for link in map[cur_link]:
-        if link.startswith("head"):
-            cur_link = link[4:]
+        for link in map[cur_link]:
+            if link.startswith("head"):
+                cur_link = link[4:]
